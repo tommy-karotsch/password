cc7b6d271e983a9db60cf39eeefc1d62b35835a6
Tommy KAROTSCH <tommy.karotsch@laplateforme.io>
2025-11-18 10:19:00 +0100

    verificateur_mdp

---
+++ password.py
@@ -0,0 +1,30 @@
+import pwinput
+import hashlib
+
+while True:
+    password = pwinput.pwinput("Veuillez entrer votre mot de passe : ")
+
+    if len(password) < 8:
+        print("Le mot de passe doit contenir au moins 8 caractères.")
+        continue
+    
+    if not any(c.isupper() for c in password):
+        print("Le mot de passe doit contenir au moins une lettre majuscule.")
+        continue
+    
+    if not any(c.islower() for c in password):
+        print("Le mot de passe doit contenir au moins une lettre minuscule.")
+        continue
+    
+    if not any(c.isdigit() for c in password):
+        print("Le mot de passe doit contenir au moins un chiffre.")
+        continue
+    
+    if not any(c in "-_.;:!?,£€()[]{}/\\!@#$%^&*" for c in password):
+        print("Le mot de passe doit contenir au moins un caractère spécial.")
+        continue
+
+    password_hache = hashlib.sha256(password.encode()).hexdigest()
+    print("Mot de passe valide !")
+    print("SHA-256 :", password_hache)
+    break
---
+++ test.py
@@ -0,0 +1,28 @@
+import random 
+
+uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase_letters = uppercase_letters.lower()
+digits = "0123456789"
+symbols = " ()[]{},;:.-_/\\?+*# "
+
+upper, lower, nums, syms = True, True ,True ,True
+
+all = ""
+
+if upper:
+    all += uppercase_letters
+
+if lower:
+    all += lowercase_letters
+
+if nums:
+    all += digits
+
+if syms: all += symbols
+
+length = 15
+amount = 15
+
+for x in range(amount):
+    password = "".join(random.choices(all, k=length))
+    print(password)
