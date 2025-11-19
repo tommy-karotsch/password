12986335571b6c0f812fd9b9834a2c8654610449
Tommy KAROTSCH <tommy.karotsch@laplateforme.io>
2025-11-19 12:02:26 +0100

    12h02

--- password_checker.py
+++ password_checker.py
@@ -2,29 +2,23 @@ import pwinput
 import hashlib
 import json
 import os
-from password_generator import generator_password
 
 FICHIER = "mots_de_passe.json"
 
-# Charger le fichier ou en créer un vide
+# 0 - Charger le fichier ou en créer un vide
 if os.path.exists(FICHIER):
     with open(FICHIER, "r") as f:
         data = json.load(f)
 else:
-    data = {}
+    data = {} # Dictionnaire vide
 
-
-
-# Sauvegarder
+# 0 - Sauvegarder
 def sauvegarder():
         with open(FICHIER, "w") as f:
              json.dump(data, f, indent=4)
              print("Mot de passe enregistré dans", FICHIER)
 
-
-
-
-# Ajouter un mot de passe
+# 1 - Ajouter un mot de passe
 def ajouter_un_mot_de_passe():
     while True: 
         password = pwinput.pwinput("Veuillez entrer votre mot de passe : ")
@@ -62,8 +56,37 @@ def ajouter_un_mot_de_passe():
         print("Mot de passe enregistré ")
         break
 
+# 2 - Generateur de mot de passe aléatoire 
+def generator_password():
+    import random
 
-# Afficher les mots de passe enregistrer
+    uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lowercase_letters = uppercase_letters.lower()
+    digits = "0123456789"
+    symbols = " ()[]{},;:.-_/\\?+*# "
+    
+    upper, lower, nums, syms = True, True ,True ,True
+
+    all = ""
+    
+    if upper:
+        all += uppercase_letters
+        
+    if lower:
+        all += lowercase_letters
+    
+    if nums:
+        all += digits
+    
+    if syms: all += symbols
+    
+    length = 12
+    amount = 1
+    for x in range(amount):
+        password = "".join(random.choices(all, k=length))
+        print(password)
+
+# 3 - Afficher les mots de passe enregistrer
 def afficher_mots_de_passe():
      if not data:
           print("Aucun mot de passe enregistré. ")
@@ -71,10 +94,9 @@ def afficher_mots_de_passe():
      print("\n--- Liste des mots de passe --- ")
      for cle, valeur in data.items():
           print(f"{cle}: {valeur}")
-          print("---------------\n")
+          print("------------------------------------------------------------------------------------\n")
 
-
-# Supprimer un mot de passe 
+# 4 - Supprimer un mot de passe 
 def supprimer_mots_de_passe():
      if not data:
           print("Aucun mot de passe à supprimer ")
@@ -98,16 +120,16 @@ def supprimer_mots_de_passe():
         sauvegarder()
         print("Mot de passe supprimé. ")
      else:
-        print("Numéro introuvable. ")   
+        print("Numéro introuvable. ")
 
-# MENU
+# ----- MENU -----
 while True:
-     print("\n=== MENU === ")
-     print("1 - Ajouter un mot de passe ")
-     print("2 - Créé un mot de passe aléatoire ")
-     print("3 - Supprimer un mot de passe  ") 
-     print("4 - Afficher tous les mots de passe ")
-     print("5 - Quitter ")
+     print("\n====== MENU ======= ")
+     print("1 -------> Ajouter un mot de passe ")
+     print("2 -------> Créé un mot de passe aléatoire ")
+     print("3 -------> Supprimer un mot de passe ") 
+     print("4 -------> Afficher tous les mots de passe ")
+     print("5 -------> Quitter ")
      
      choix = input("Choix : ")
      if choix == "1":
@@ -122,7 +144,4 @@ while True:
           break
      else:
         print("Choix invalide. ")
-
-
-
 
